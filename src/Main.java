fa0f2780c228303f179d88b51b0a8b0a108c920e
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-08-03 00:14:27 +0300

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Zoo zoo = new Zoo();
+
+        //Hayvanlari ekleyelim
+        Animal lion = new Animal("lion", 12 );
+        Animal eagle = new Animal("eagle", 5);
+
+        zoo.addAnimal(lion);
+        zoo.addAnimal(eagle);
+
+        //calisanlari ekleyelim
+        Employee zookeeper = new Employee("Jack",101);
+        Employee vet= new Employee("Jack",101);
+
+        zoo.addEmployee(zookeeper);
+        zoo.addEmployee(vet);
+
+        //hayvanlari ve calisanlari gosterelim
+        zoo.showAnimal();
+        zoo.showEmployees();
+
+        //calisanlarin calismasini gosterelim
+        zookeeper.work();
+        vet.work();
+
+        //hayvanlarin ses cika
+
+
+    }
+}
+
+
+
