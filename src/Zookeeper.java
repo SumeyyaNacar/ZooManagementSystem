8c553fea437274beea985588a6fe06c3d565ee44
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-08-03 00:15:22 +0300

    Create Zookeper.java

---
+++ src/Zookeeper.java
@@ -0,0 +1,11 @@
+public class Zookeeper extends Employee{
+    public Zookeeper(String name, int id) {
+        super(name, id);
+    }
+
+
+    @Override
+    public void work() {
+        System.out.println(getName() + "is taking care of animals");
+    }
+}
